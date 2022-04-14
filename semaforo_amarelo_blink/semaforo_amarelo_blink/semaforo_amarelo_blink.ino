ea75d3aa424a6fbc421e922114b362091b353f85
Bruno Marzano <umarzzo92@gmail.com>
2022-04-14 15:41:28 -0300

    Update semaforo_amarelo_blink

--- semaforo_amarelo_blink/semaforo_amarelo_blink/semaforo_amarelo_blink.ino
+++ semaforo_amarelo_blink/semaforo_amarelo_blink/semaforo_amarelo_blink.ino
@@ -1,9 +1,11 @@
+// Declarando nome e posição dos LED's:
 int vermelho = 13;
 int amarelo = 9;
 int verde = 6;
 
 void setup() {
-
+  
+//Setando os pinos como Saída:
   pinMode(verde, OUTPUT);
   pinMode(amarelo, OUTPUT);
   pinMode(vermelho, OUTPUT);
@@ -12,23 +14,23 @@ void setup() {
 
 void loop() {
 
-  digitalWrite(vermelho, LOW);
+  digitalWrite(vermelho, LOW); // Apaga LED vermelho
   
     for(int a = 0; a <= 5; a++){
     digitalWrite(amarelo, HIGH);
     delay(500);
     digitalWrite(amarelo, LOW);
     delay(500);
-  }
+  } // Faz LED amarelo piscar por um período determinado
   
-   digitalWrite(verde, LOW);
+   digitalWrite(verde, LOW); // Apaga LED verde
   
 
-  digitalWrite(amarelo, LOW);
-  digitalWrite(vermelho, HIGH);
-  delay(10000);
+  digitalWrite(amarelo, LOW); // Apaga LED amarelo
+  digitalWrite(vermelho, HIGH); // Acende LED vermelho
+  delay(10000); // Duração
 
-  digitalWrite(vermelho, LOW);
-  digitalWrite(verde, HIGH);
-  delay(10000);
+  digitalWrite(vermelho, LOW); // Apaga LED vermelho
+  digitalWrite(verde, HIGH); // Acende LED verde
+  delay(10000); // Duração
 }
